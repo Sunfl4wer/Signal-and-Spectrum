7d241f8f1630851084883503037d595509c20a2a
Helianthus <35807998+Sunfl4wer@users.noreply.github.com>
2019-09-15 11:49:23 +0700

    update

--- MATLAB/signal_and_spectrum.m
+++ MATLAB/signal_and_spectrum.m
@@ -1,3 +1,9 @@
+% Ch??ng trình MATLAB v? tín hi?u và ph? trong th?i gian th?c
+% Gi?i thu?t ???c trình bày ? dòng 80 c?a script.
+% Sinh viên th?c hi?n: Nguy?n Thái S?n - 1512847
+%                      Nguy?n Minh Hùng - 1511355
+% H? Chí Minh - 09/2019
+
 function varargout = signal_and_spectrum(varargin)
 % SIGNAL_AND_SPECTRUM MATLAB code for signal_and_spectrum.fig
 %      SIGNAL_AND_SPECTRUM, by itself, creates a new SIGNAL_AND_SPECTRUM or raises the existing
@@ -73,11 +79,12 @@ function varargout = signal_and_spectrum_OutputFcn(hObject, eventdata, handles)
 varargout{1} = handles.output;
 
 
-% --- Executes on button press in pushbutton1.
+%% --- Executes on button press in pushbutton1.
 function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+%% Khai báo các bi?n toàn c?c các bi?n này có th? ???c dùng trong nhi?u function khác nhau.
     global f;
     global audio;
     global freq_db;
@@ -87,17 +94,60 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     global fs;
     global spf;
     global micReader;
-    fs = 44100;
-    spf = 1024;
-    micReader = audioDeviceReader(fs,spf);
-    f = (0:(spf-1))*fs/spf;
-    mark = 1;
+    global audio_windowed;
+    global window;
+    global audio_first_part;
+    global audio_second_part;
+  %% Thi?t ??t các giá tr? ??u
+    fs = 44100; % t?n s? l?y m?u
+    spf = 1024; % s? m?u m?t m?ng
+    window = 30; % s? m?u s? d?ng l?i c?a c?a s? tr??t
+    micReader = audioDeviceReader(fs,spf); % khai báo hàm l?y tín hi?u t? mic máy tính
+    f = (0:(spf-1))*fs/spf; % t?o d?i t?n s?
+    mark = 1; % bi?n xác nh?n ch?y vòng l?p khi b?m nút
+    % t?o các m?ng c?n thi?t
     audio = zeros(spf,1);
+    audio_windowed = zeros(spf,1);
+    audio_first_part = zeros(window,1);
+    audio_second_part = zeros(spf-window,1);
     freq = zeros(spf,1);
     freq_abs = zeros(spf,1);
     freq_db = zeros(spf,1);
+    k = 1;
+    %% Vòng l?p vô h?n th?c hi?n gi?i thu?t
         while(mark)
-            audio = micReader();
+            audio = micReader(); % l?y d? li?u thu ???c t? mic gán vào m?ng 'audio'
+            %----------------------------------------------------------------------
+            % Gi?i thu?t dùng ?? t?o c?a s? tr??t
+            % Bi?n d? li?u 'audio_windowed' s? g?m n+m giá tr? v?i n là s? các d? li?u l?y
+            % t? m?ng 'audio' th? i-1 và m là s? các d? li?u l?y t? m?ng
+            % 'audio' th? i.
+            % n = window và m = sample_per_frame-window
+            % M?ng 'audio_first_part' s? gi? ph?n ??u c?a d? li?u m?i
+            % M?ng 'audio_second_part' s? gi?a ph?n sau c?a d? li?u m?i
+            % audio_windowed = [audio_first_part,audio_second_part]
+            if(k == 1)
+                audio_windowed = audio;
+                for i = 1:window
+                    audio_first_part(i) = audio(spf-window+i);
+                end
+                k = 2;
+            else
+                for j = 1:spf-window
+                    audio_second_part(j) = audio(j);
+                end
+                for l = 1:window
+                    audio_windowed(l) = audio_first_part(l);
+                    audio_first_part(l) = audio(spf-window+l);
+                end
+                for t = 1:spf-window
+                    audio_windowed(t+window) = audio_second_part(t);
+                end
+            end
+            %--------------------------------------------------------------
+            % V? d?ng c?a tín hi?u
+            % Do tín hi?u g?m 1024 m?u v?i t?c ?? l?y m?u là 44100Hz nên
+            % th?i gian hi?n th? lên khung hình s? t? 0 ??n 1024/44100s
                 axes(handles.axes1);
                 plot((0:1/fs:(spf-1)/fs),audio);
                     ylim('manual');
@@ -105,11 +155,17 @@ function pushbutton1_Callback(hObject, eventdata, handles)
                     ylabel('amplitude');
                     drawnow;         
                 
-                
+                %----------------------------------------------------------
+                % Tính fft c?a tín hi?u
+                % Do tín hi?u là k?t qu? c?a c?a s? tr??t nên khi tính fft
+                % c?a tín hi?u này thì t??ng ?ng v?i vi?c dùng hàm stft.
                 freq = fft(audio);
                 freq_abs = abs(freq);
-                freq_db = 20*log(freq_abs);
+                freq_db = 20*log10(freq_abs);
                 
+                %----------------------------------------------------------
+                % V? d?ng c?a phân tích fourier c?a tín hi?u trên mi?n t?n
+                % s?.
                 axes(handles.axes2);
                 plot(f(1:(spf/2)),freq_db(1:(spf/2)));
                     xlim('manual');
@@ -132,6 +188,7 @@ function pushbutton2_Callback(hObject, eventdata, handles)
     global f;
     global spf;
     global fs;
+    global audio_windowed;
     mark = 0;
     axes(handles.axes1);
     plot((0:1/fs:(spf-1)/fs),audio);
--- MATLAB/spectrogram_app.m
+++ MATLAB/spectrogram_app.m
@@ -1,3 +1,9 @@
+% Ch??ng trình MATLAB v? tín hi?u và ph? trong th?i gian th?c
+% Gi?i thu?t ???c trình bày ? dòng 80 c?a script.
+% Sinh viên th?c hi?n: Nguy?n Thái S?n - 1512847
+%                      Nguy?n Minh Hùng - 1511355
+% H? Chí Minh - 09/2019
+
 function varargout = spectrogram_app(varargin)
 % SPECTROGRAM_APP MATLAB code for spectrogram_app.fig
 %      SPECTROGRAM_APP, by itself, creates a new SPECTROGRAM_APP or raises the existing
@@ -78,6 +84,7 @@ function pushbutton1_Callback(hObject, eventdata, handles)
 % hObject    handle to pushbutton1 (see GCBO)
 % eventdata  reserved - to be defined in a future version of MATLAB
 % handles    structure with handles and user data (see GUIDATA)
+%% Khai báo các bi?n toàn c?c các bi?n này có th? ???c dùng trong nhi?u function khác nhau.
     global sample_rate;
     global sample_per_frame;
     global frame_stack;
@@ -92,11 +99,20 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     global i;
     global audio;
     global micReader;
+    global audio_windowed;
+    global window;
+    global audio_first_part;
+    global audio_second_part;
+    %% Thi?t ??t các giá tr? ??u
+    audio_windowed = zeros(sample_per_frame,1);
+    audio_first_part = zeros(window,1);
+    audio_second_part = zeros(sample_per_frame-window,1);
     mark = 1;
     sample_rate = 44100;
     sample_per_frame = 1024;
+    window = 30;
     micReader = audioDeviceReader(sample_rate,sample_per_frame);
-    frame_stack = 20;
+    frame_stack = 50;
     i = 1;
     f = (0:(sample_per_frame-1))*sample_rate/sample_per_frame;
     stack = zeros(frame_stack,(sample_per_frame/2));
@@ -105,16 +121,61 @@ function pushbutton1_Callback(hObject, eventdata, handles)
     for m = 1:frame_stack
         for n = 1:(sample_per_frame/2)
             X(m,n) = f(n);
-            Y(m,n) = m;
+            Y(m,n) = m*(sample_per_frame - window)/44100;
         end
     end
     axes(handles.axes1);
     % Process frame-by-frame in a loop
+    k = 1;
+    %% Vòng l?p vô h?n th?c hi?n gi?i thu?t
     while(mark)
-        audio = micReader();
-         freq = fft(audio);
+        audio = micReader();% l?y d? li?u thu ???c t? mic gán vào m?ng 'audio'
+            %----------------------------------------------------------------------
+            % Gi?i thu?t dùng ?? t?o c?a s? tr??t
+            % Bi?n d? li?u 'audio_windowed' s? g?m n+m giá tr? v?i n là s? các d? li?u l?y
+            % t? m?ng 'audio' th? i-1 và m là s? các d? li?u l?y t? m?ng
+            % 'audio' th? i.
+            % n = window và m = sample_per_frame-window
+            % M?ng 'audio_first_part' s? gi? ph?n ??u c?a d? li?u m?i
+            % M?ng 'audio_second_part' s? gi?a ph?n sau c?a d? li?u m?i
+            % audio_windowed = [audio_first_part,audio_second_part]
+            if(k == 1)
+                audio_windowed = audio;
+                for r = 1:window
+                    audio_first_part(r) = audio(sample_per_frame-window+r);
+                end
+                k = 2;
+            else
+                for j = 1:sample_per_frame-window
+                    audio_second_part(j) = audio(j);
+                end
+                for l = 1:window
+                    audio_windowed(l) = audio_first_part(l);
+                    audio_first_part(l) = audio(sample_per_frame-window+l);
+                end
+                for t = 1:sample_per_frame-window
+                    audio_windowed(t+window) = audio_second_part(t);
+                end
+            end
+         %-----------------------------------------------------------------
+         % V? d?ng c?a tín hi?u
+         % Tính fft c?a tín hi?u
+         % Do tín hi?u là k?t qu? c?a c?a s? tr??t nên khi tính fft
+         % c?a tín hi?u này thì t??ng ?ng v?i vi?c dùng hàm stft.
+         freq = fft(audio_windowed);
          freq_abs = abs(freq);
-         freq_db = 20*log(freq_abs);
+         freq_db = 20*log10(freq_abs);
+         
+         %-----------------------------------------------------------------
+         % Ma tr?n frame_stack là ma tr?n nxm v?i n = stack và 
+         % m = sample_per frame.
+         % V?i m?i l?n tính fft c?a m?ng giá tr? thu ???c thì k?t qu? s?
+         % ???c ??y vào stack. Sau khi stack ??y thì frame ??u s? b? lo?i
+         % b?, các frame còn l?i s? d?ch lùi 1 v? trí và frame cu?i s? ???c
+         % c?p nh?t l?i giá tr? m?i.
+         % frame_stack s? ???c dùng ?? v? ?? th? 3d theo th?i gian c?a ph?
+         % c?a tín hi?u. 
+         % Th?i gian gi?a m?i frame s? b?ng (sample_per_frame-window)/44100
             if(i <= frame_stack)
                 for j = 1:(sample_per_frame/2)
                     stack(i,j) = freq_db(j);
@@ -130,6 +191,9 @@ function pushbutton1_Callback(hObject, eventdata, handles)
                     stack(frame_stack,k) = freq_db(k);
                 end
             end
+            
+        %------------------------------------------------------------------
+        % Dùng hàm surf ?? v? tín hi?u
         surf(Y,X,stack);
         zlim([-150 150])
         colorbar;
